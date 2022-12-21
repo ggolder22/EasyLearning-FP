4a509a99a3d189391f950688cbf9fa47b551dcbb
ggolder22 <germanxvzy985@hotmail.com>
2022-12-20 22:21:54 -0300

    intento deploy

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { test } = require("./src/Routes/test/controllers.js");
+const PORT = require("./src/config.js")
 
 conn.sync({ force: false }).then(async () => {
   await test();
-  server.listen(3001, () => {
-    console.log(`Server listening at ${3001}`);
+  server.listen(PORT, () => {
+    console.log(`Server listening at ${PORT}`);
   });
   // await axios.post("http://localhost:3001/test");
 });
